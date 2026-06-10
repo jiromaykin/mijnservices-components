57bbff76b0d3a21b23f257353317e3c16b6270be
Jiro Ghianni <118177951+jiromaykin@users.noreply.github.com>
2026-06-10 14:52:22 +0200

    refactor(card): extract inline styles to SCSS mixins

--- components/card/src/card.ts
+++ components/card/src/card.ts
@@ -1,70 +1,10 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import styles from './index.scss?inline';
 
 @customElement('mijnservices-card')
 export class MijnservicesCard extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-    }
-
-    .mijnservices-card {
-      position: relative;
-      background-color: var(--mijnservices-card-background-color, #ddeeff);
-      border-color: var(--mijnservices-card-border-color, #b3d4f0);
-      border-style: solid;
-      border-width: var(--mijnservices-card-border-width, 1px);
-      border-radius: var(--mijnservices-card-border-radius, 8px);
-      padding-block: var(--mijnservices-card-padding-block, 1.5rem);
-      padding-inline: var(--mijnservices-card-padding-inline, 1.5rem);
-    }
-
-    /* De link bedekt de hele card */
-    .mijnservices-card__action::before {
-      content: '';
-      position: absolute;
-      inset: 0;
-    }
-
-    .mijnservices-card__domain {
-      color: var(--mijnservices-card-domain-color, #1a4a6e);
-      font-size: var(--mijnservices-card-domain-font-size, 0.875rem);
-      font-weight: var(--mijnservices-card-domain-font-weight, 400);
-      margin-block: 0;
-    }
-
-    .mijnservices-card__heading {
-      color: var(--mijnservices-card-heading-color, #0d2d4a);
-      font-size: var(--mijnservices-card-heading-font-size, 1.25rem);
-      font-weight: var(--mijnservices-card-heading-font-weight, 700);
-      margin-block: var(--mijnservices-card-heading-margin-block, 0.25rem 0);
-    }
-
-    .mijnservices-card__footer {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      margin-block-start: var(--mijnservices-card-footer-margin-block-start, 2rem);
-    }
-
-    .mijnservices-card__date {
-      color: var(--mijnservices-card-date-color, #1a4a6e);
-      font-size: var(--mijnservices-card-date-font-size, 0.875rem);
-    }
-
-    .mijnservices-card__action {
-      color: var(--mijnservices-card-action-color, #1a6eb4);
-      margin-inline-start: auto;
-      text-decoration: none;
-      display: flex;
-      align-items: center;
-    }
-
-    .mijnservices-card__arrow {
-      block-size: 1.5rem;
-      inline-size: 1.5rem;
-    }
-  `;
+  static styles = unsafeCSS(styles);
 
   @property() heading = '';
   @property() domain = '';
